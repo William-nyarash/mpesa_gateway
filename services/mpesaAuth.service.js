afb823bc0eb2bf82ec28253c1a5f06cd924703ed
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-08-05 17:09:40 +0300

    no change

--- services/mpesaAuth.service.js
+++ services/mpesaAuth.service.js
@@ -4,7 +4,6 @@ const path = require("path");
 const logger = require("../utils/logger");
 
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-require("dotenv").config();
 
 const generateAccessToken = async () => {
   const consumerKey = process.env.MPESA_CONSUMER_KEY;
@@ -12,17 +11,14 @@ const generateAccessToken = async () => {
 
   const url =
     "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-
   try {
     const encodedCredentials = Buffer.from(
       `${consumerKey}:${consumerSecret}`
     ).toString("base64");
-
     const headers = {
       Authorization: `Basic ${encodedCredentials}`,
       "Content-Type": "application/json",
     };
-
     const response = await axios.get(url, { headers });
     const token = response.data.access_token;
     return token;
